82181e37ac564a490a58957160440a7cd40cda3d
gudluck123 <rahulkumarkiit94@gmail.com>
2022-09-05 16:55:10 +0530

    15:5

---
+++ src/Routes/walletRoute.js
@@ -0,0 +1,9 @@
+const router = require('express').Router()
+
+const {createWallet} = require('../Controller/walletController')
+
+
+router.post('/createWallet', createWallet)
+
+module.exports = router;
+
